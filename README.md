a454ab090ffe119d1f518754114a2fa48d08f7d7
sametkoyuncu <sametkoyuncu@live.com>
2020-12-30 23:13:46 +0300

    Added new function

--- README.md
+++ README.md
@@ -11,6 +11,25 @@ Get the number of days (or seconds, minutes etc.) between two dates in JavaScrip
 const dateween = require('dateween');
 
 console.log(dateween.getDays("2021-12-3", "2020-1-24"));
+/*
+    output -> 679
+*/
+
+console.log(dateween.getAllInOne("2021-12-3", "2020-1-24"));
+/*
+    output ->
+    AllInOne {
+        year: 1.8602739726027397,
+        month: 22.633333333333333,
+        week: 97,
+        day: 679,
+        minute: 977760,
+        second: 58665600,
+        millisecond: 58665600000,
+        microsecond: 58665600000000,
+        nanosecond: 58665600000000000
+    }
+*/
 ```
 # All Functions
 ```
@@ -24,4 +43,5 @@ getSeconds
 getMilliseconds
 getMicroseconds
 getNanoseconds
+getAllInOne //return object
 ```
--- index.js
+++ index.js
@@ -43,6 +43,34 @@ function getNanoseconds(date1, date2) {
     return (turnTimeStamp(date1)-turnTimeStamp(date2)) * (1000 * 1000)
 }
 
+function getAllInOne(date1, date2) {
+    return new AllInOne(
+        getYears(date1, date2),
+        getMonths(date1, date2),
+        getWeeks(date1, date2),
+        getDays(date1, date2),
+        getMinutes(date1, date2),
+        getSeconds(date1, date2),
+        getMilliseconds(date1, date2),
+        getMicroseconds(date1, date2),
+        getNanoseconds(date1, date2)
+    );
+}
+
+class AllInOne {
+    constructor(year, month, week, day, minute, second, millisecond, microsecond, nanosecond) {
+        this.year = year;
+        this.month = month;
+        this.week = week;
+        this.day = day;
+        this.minute = minute;
+        this.second = second;
+        this.millisecond = millisecond;
+        this.microsecond = microsecond;
+        this.nanosecond = nanosecond;
+    }
+}
+
 module.exports = {
     getYears, 
     getMonths, 
@@ -53,5 +81,6 @@ module.exports = {
     getSeconds,
     getMilliseconds,
     getMicroseconds,
-    getNanoseconds
+    getNanoseconds,
+    getAllInOne
 }
